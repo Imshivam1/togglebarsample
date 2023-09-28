b1ffdd9319ab19d9e5f2a48b3eab88916ade47e9
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2023-09-28 16:37:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+const toggleSwitch = document.getElementById('toggleSwitch');
+const switchdiv = document.querySelector('.switch');
+const textLine = document.querySelector('h1')
+
+toggleSwitch.addEventListener('change', function(){
+  if (toggleSwitch.checked){
+    document.body.style.backgroundColor='black';
+    switchdiv.style.backgroundColor='black';
+    switchdiv.style.transform = 'translateX(50%)';
+    textLine.style.color = 'white';
+
+  } else{
+    document.body.style.backgroundColor='white';
+    switchdiv.style.backgroundColor='black';
+    switchdiv.style.transform = 'translateX(0)';
+    textLine.style.color = 'black';
+  }
+});
